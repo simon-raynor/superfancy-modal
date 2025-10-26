e29df7fa800fcd4cac59cab1cd3fcfdd083fe62e
Simon Raynor <ssraynor@googlemail.com>
2025-10-26 20:09:55 +0000

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,46 @@
+import './style.css'
+
+function init() {
+    const btns = document.querySelectorAll('.btns button');
+    const modal = document.getElementById('modal')!;
+
+    function openModal(evt: Event) {
+        if (evt instanceof PointerEvent) {
+            modal.style.setProperty('--evtx-in', `${evt.pageX - window.scrollX}px`);
+            modal.style.setProperty('--evty-in', `${evt.pageY - window.scrollY}px`);
+        }
+        const classname = (evt.target as HTMLElement).getAttribute('data-modal')!;
+        requestAnimationFrame(_ => {
+            modal.classList.add('open', 'active', classname);
+            modal.setAttribute('data-modal', classname)
+        });
+    }
+
+    function closeModal(evt: Event) {
+        if (evt instanceof PointerEvent) {
+            modal.style.setProperty('--evtx-out', `${evt.pageX - window.scrollX}px`);
+            modal.style.setProperty('--evty-out', `${evt.pageY - window.scrollY}px`);
+        }
+
+        modal.classList.remove('open');
+
+        const ac = new AbortController();
+
+        const cleanup = () => {
+            const classname = modal.getAttribute('data-modal')!;
+            modal.classList.remove('active', classname);
+            ac.abort();
+        }
+
+        modal.addEventListener('animationend', cleanup, { signal: ac.signal });
+
+        modal.addEventListener('transitionend', cleanup, { signal: ac.signal });
+    }
+
+    btns.forEach(btn => {
+        btn.addEventListener('click', openModal)
+    });
+    modal.querySelector('button')?.addEventListener('click', closeModal);
+}
+
+document.addEventListener('DOMContentLoaded', init);
